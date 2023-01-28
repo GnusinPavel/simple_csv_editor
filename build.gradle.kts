54b9b24205500a0cf86a79529e3c560e6c59a0af
Pavel Gnusin <pavel.sin.gnusin@gmail.com>
2023-01-28 18:04:29 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.github.gnusin_pavel"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.github.gnusin_pavel.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "simple_csv_editor"
+            packageVersion = "1.0.0"
+        }
+    }
+}
